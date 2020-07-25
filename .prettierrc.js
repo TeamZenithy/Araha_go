271927891743eaa3a32364343c5d9167adc291d2
B1ackAnge1 <30624945+B1ackAnge1@users.noreply.github.com>
2020-07-25 20:50:43 +0900

    Initial Commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  jsxSingleQuote: true,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
