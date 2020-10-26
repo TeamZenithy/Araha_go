e46a267cce3339d3278346b057f01ce11a21c447
aroxu <30624945+aroxu@users.noreply.github.com>
2020-10-26 11:57:24 +0900

    Update play.go

--- commands/music/play/play.go
+++ commands/music/play/play.go
@@ -5,8 +5,9 @@ import (
 	audioengine "github.com/TeamZenithy/Araha/engine/audio"
 	"github.com/TeamZenithy/Araha/handler"
 	"github.com/TeamZenithy/Araha/utils"
-	"io/ioutil"
+	"github.com/bwmarrin/discordgo"
 	"log"
+	"time"
 )
 
 func Initialize() {
@@ -15,37 +16,31 @@ func Initialize() {
 			Run:                  run,
 			Names:                []string{commandName},
 			RequiredArgumentType: []string{commandArg},
-			Usage:                map[string]string{"필요한 권한":"**``음성 채널 발언권``**", "설명":"``요청된 이름의 노래 또는 링크를 검색해서 음원을 재생합니다.``", "사용법": "```css\n?!ping 노래 이름 또는 링크```"},
+			Usage:                map[string]string{"필요한 권한": "**``음성 채널 발언권``**", "설명": "``요청된 이름의 노래 또는 링크를 검색해서 음원을 재생합니다.``", "사용법": "```css\n?!ping 노래 이름 또는 링크```"},
 		},
 	)
 }
 
 const (
 	commandName = "play"
-	commandArg = "없음"
+	commandArg  = "노래 이름 또는 링크"
 
-	QUERY_TYPE_YOUTUBE= "ytsearch"
+	QUERY_TYPE_YOUTUBE    = "ytsearch"
 	QUERY_TYPE_SOUNDCLOUD = "scsearch"
 )
 
 func run(ctx handler.CommandContext) error {
-	rawConfig, errFindConfigFile := ioutil.ReadFile("config.toml") // just pass the file name
-	if errFindConfigFile != nil {
-		log.Fatalln("Error while load config file: " + errFindConfigFile.Error())
-		return nil
-	}
-	errLoadConfigData, prefix := utils.GetPrefix(string(rawConfig))
-	if errLoadConfigData != nil {
-		log.Fatalln("Error while load config data: " + errLoadConfigData.Error())
-	}
-	query := ctx.Message.Content[len(prefix) + len(commandName) + 1:]
 	node, errBestNode := utils.Lavalink.BestNode()
 	if errBestNode != nil {
 		log.Println(errBestNode)
 		return nil
 	}
-	ctx.Message.Reply("🔎 " + query + "을(를) 찾는중...")
-	tracks, errLoadTracks := node.LoadTracks("ytsearch", query)
+	if ctx.Arguments[commandArg] == "" {
+		ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, "❌ 검색어 또는 링크를 입력해주세요.")
+		return nil
+	}
+	searchingMsg, _ := ctx.Message.Reply("🔎 " + ctx.Arguments[commandArg] + "을(를) 찾는중...")
+	tracks, errLoadTracks := node.LoadTracks(QUERY_TYPE_YOUTUBE, ctx.Arguments[commandArg])
 	if errLoadTracks != nil {
 		log.Println(errLoadTracks)
 		return nil
@@ -55,14 +50,44 @@ func run(ctx handler.CommandContext) error {
 	}
 	if tracks.Type == audioengine.NoMatches {
 		fmt.Println("NO Result")
+		ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, "❌ 일치하는 검색 결과가 없습니다.")
 		return nil
 	}
+
 	track := tracks.Tracks[0].Data
+
 	errPlay := utils.Player.Play(track)
-	ctx.Message.Reply("Track Info: ```json\n"+fmt.Sprintf("%b", tracks.Tracks[0].Info)+"```")
 	if errPlay != nil {
 		log.Println(errPlay)
 		return nil
 	}
+	embed := &discordgo.MessageEmbed{
+		Author: &discordgo.MessageEmbedAuthor{},
+		Color:  39423,
+		Title:  "✅ 노래가 추가되었습니다.",
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{
+				Name:   "제목",
+				Value:  tracks.Tracks[0].Info.Title,
+				Inline: true,
+			}, &discordgo.MessageEmbedField{
+				Name:   "업로더",
+				Value:  tracks.Tracks[0].Info.Author,
+				Inline: true,
+			}, &discordgo.MessageEmbedField{
+				Name:   "링크",
+				Value:  tracks.Tracks[0].Info.URI,
+				Inline: true,
+			}, &discordgo.MessageEmbedField{
+				Name:   "신청자",
+				Value:  "<@" + ctx.Message.Author.ID + ">",
+				Inline: true,
+			},
+		},
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
+	ctx.Session.ChannelMessageDelete(searchingMsg.ChannelID, searchingMsg.ID)
+	ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID, embed)
+
 	return nil
 }
