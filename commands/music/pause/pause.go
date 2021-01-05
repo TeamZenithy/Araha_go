8602fef16a855869af047ce95d6bd4b401157194
gangjun06 <me@gangjun.dev>
2021-01-05 12:43:52 +0900

    pause/resume

---
+++ commands/music/pause/pause.go
@@ -0,0 +1,58 @@
+package pause
+
+import (
+	"github.com/TeamZenithy/Araha/extensions/embed"
+	"github.com/TeamZenithy/Araha/handler"
+	"github.com/TeamZenithy/Araha/model"
+	"github.com/TeamZenithy/Araha/utils"
+	"github.com/bwmarrin/discordgo"
+)
+
+//Initialize command
+func Initialize() {
+	handler.AddCommand(
+		handler.Command{
+			Run:                  run,
+			Name:                 commandName,
+			Aliases:              []string{""},
+			RequiredArgumentType: []string{commandArg},
+			Category:             utils.CATEGORY_MUSIC,
+			Description:          &handler.Description{ReqPermsission: "SPEAK", Usage: "pause"},
+		},
+	)
+}
+
+const (
+	commandName = "pause"
+	commandArg  = "none"
+)
+
+func run(ctx handler.CommandContext) error {
+	e := embed.New(ctx.Session, ctx.Message.ChannelID)
+	guild, err := ctx.Session.State.Guild(ctx.Message.GuildID)
+	if err != nil {
+		return nil
+	}
+
+	var userVoiceState discordgo.VoiceState
+	for _, vs := range guild.VoiceStates {
+		if vs.UserID == ctx.Message.Author.ID {
+			userVoiceState = *vs
+		}
+	}
+	if userVoiceState.UserID == "" {
+		e.SendEmbed(embed.BADREQ, ctx.T("music:NotInVoiceChannel"))
+		return nil
+	}
+	ms, ok := model.Music[ctx.Message.GuildID]
+	if ok && len(ms.Queue) > 0 {
+		if ms.Player.Paused() {
+			e.SendEmbed(embed.BADREQ, ctx.T("music:AlreadyPaused"))
+		} else {
+			e.SendEmbed(embed.BADREQ, ctx.T("music:Paused"))
+		}
+	} else {
+		e.SendEmbed(embed.BADREQ, ctx.T("music:NoMusic"))
+	}
+	return nil
+}
